da60612bdbd5a3c7cda43c16bf5700ddd79819a1
Erik Mellum <mellum.erik@gmail.com>
2025-10-15 16:17:42 -0700

    Add RBAC to graphql API

--- packages/csci32-database/src/client.ts
+++ packages/csci32-database/src/client.ts
@@ -1,4 +1,8 @@
-import { PrismaClient } from '../generated/prisma/client'
+export * from '../prisma/seeders/seedRoles'
+export * from '../prisma/seeders/seedPermissions'
+export * from '../prisma/seeders/seedUsers'
+
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
@@ -6,4 +10,4 @@ export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export * from '../generated/prisma/client'
+export * from '@prisma/client'
